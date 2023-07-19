ff211e6029f49bc3e361ba16d5b03132d9e18f3f
SenorKotek <kwadunkx@yahoo.com>
2023-07-20 01:03:07 +0300

    fix pep8

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from users.models import User, Subscriptions
+
+from .models import Subscriptions, User
 
 
 @admin.register(User)
